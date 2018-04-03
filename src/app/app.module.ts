adb8904c8177be451ed86171d87b2506f953b473
Adonay Santiago Espinosa Montaño <adonayespinosa@outlook.com>
2018-04-03 00:21:18 -0500

    Dynamic Forms working. Good base template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,30 +5,44 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { FormBuilderPage } from './../pages/form-builder/form-builder';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { DynamicFormComponent } from '../components/dynamic-form/dynamic-form.component';
+import { DynamicFormQuestionComponent } from './../components/dynamic-form-question/dynamic-form-question.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { QuestionControlService } from '../ts/question-control.service';
+import { QuestionService } from '../ts/question.service';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    FormBuilderPage,
+    DynamicFormComponent,
+    DynamicFormQuestionComponent
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    FormBuilderPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+
+    QuestionService,
+    QuestionControlService
   ]
 })
 export class AppModule {}
