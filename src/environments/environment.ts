a11cbc7904cc29abb41a52629385e3613168fadf
Adonay Espinosa <adonay.espinosa1@gmail.com>
2018-04-20 15:29:50 +0000

    added ionic environment variables for firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: '<your-key>',
-    authDomain: '<your-project-authdomain>',
-    databaseURL: '<your-database-URL>',
-    projectId: '<your-project-id>',
-    storageBucket: '<your-storage-bucket>',
-    messagingSenderId: '<your-messaging-sender-id>'
+    apiKey: "AIzaSyC9UlbpfXs2C2yT38_rIqU_elgj-rlrL4Y",
+    authDomain: "pftry-77240.firebaseapp.com",
+    databaseURL: "https://pftry-77240.firebaseio.com",
+    projectId: "pftry-77240",
+    storageBucket: "pftry-77240.appspot.com",
+    messagingSenderId: "375944095749"
   }
 };
