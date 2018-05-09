a9a719620b1677c3d4ac93a7c1c0eb9e5a77e338
Adonay Espinosa <adonay.espinosa1@gmail.com>
2018-05-09 12:14:17 -0500

    valor default para textbox editado

--- src/ts/question-textbox.ts
+++ src/ts/question-textbox.ts
@@ -6,6 +6,6 @@ export class TextboxQuestion extends QuestionBase<string> {
 
   constructor(options: {} = {}) {
     super(options);
-    this.type = options['type'] || '';
+    this.type = options['type'] || 'text';
   }
 }
