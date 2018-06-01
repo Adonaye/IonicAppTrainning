60ece4818fee7603d609b5e9461f425a76d631cb
Adonay Espinosa <adonay.espinosa1@gmail.com>
2018-06-01 09:32:18 -0500

    Agregada confirmacion antes de salir del formulario

--- src/pages/form-viewer/form-viewer.ts
+++ src/pages/form-viewer/form-viewer.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
 import { QuestionService } from '../../ts/question.service';
 import { FormularioInterface } from '../../models/formulario';
 import { CategoriaInterface } from '../../models/categoria';
@@ -26,13 +26,16 @@ export class FormViewerPage {
   categoria: CategoriaInterface;
   formulario: FormularioInterface;
   responseCallback: Function;
+  canLeave: boolean;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    qService: QuestionService,
+    private alertCtrl: AlertController,
+    private qService: QuestionService,
     public formProv: FormulariosProvider
   ) {
+    this.canLeave = false;
     this.categoria = this.navParams.get('categoria');
     this.formulario = this.navParams.get('formulario');
     this.questions = this.initQuestions(this.formulario.Campos);
@@ -55,9 +58,39 @@ export class FormViewerPage {
       }
     }).sort((a, b) => a.order - b.order);
   }
+  
+  presentConfirm() {
+    let alert = this.alertCtrl.create({
+      title: 'Confirmar salida del formulario',
+      message: '¿Está seguro que desea salir de la ventana? Se perderá toda la información no guardada',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            this.canLeave = false;
+          }
+        },
+        {
+          text: 'Aceptar',
+          handler: () => {
+            this.canLeave = true;
+            this.navCtrl.popToRoot();
+          }
+        }
+      ]
+    });
+    return alert.present();
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad FormViewerPage');
+  }
+
+  ionViewCanLeave() {
+    if(!this.canLeave)
+        this.presentConfirm();
+    return this.canLeave;
   }
 
 }
